b198d6e79c72279d86b7edfa661d3f845445ea3e
Simon Burfield <simon.burfield@o2.com>
2017-03-25 19:31:00 +0000

    Logging

--- scripts/vextank.py
+++ scripts/vextank.py
@@ -87,6 +87,8 @@ def scan_publisher():
         # us.publish(UInt16(range))
         # rospy.loginfo("SCAN:" + str(range))
 
+        rospy.loginfo("ODOM:" + str(BrickPi.Encoder[LF_WHEEL]) + str(BrickPi.Encoder[RF_WHEEL]) )
+
         left_encoder = rospy.Publisher('lwheel', Int16, queue_size=10)
         right_encoder = rospy.Publisher('rwheel', Int16, queue_size=10)
         left_encoder.publish(BrickPi.Encoder[LF_WHEEL])
